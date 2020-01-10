c07528b1643a15254ca6f6062f4b2cce01a07ee4
Jampottie <j.mooibroek2002@gmail.com>
2020-01-10 13:11:32 +0100

    Example

--- server/index.php
+++ server/index.php
@@ -1,4 +1,21 @@
 <?php 
-$API_KEY = "0zS0ftbUTfv7o1KOlBSoTgKbkYg1ft5u";
-$API_SECRET =  "DVCymhTIoHwLSxqBM9E8riDIxifCQCOS";
+$apiKey = "0zS0ftbUTfv7o1KOlBSoTgKbkYg1ft5u";
+$apiSecret =  "DVCymhTIoHwLSxqBM9E8riDIxifCQCOS";
+$host = 'https://api-cn.faceplusplus.com';
+
+include_once 'FppClient.php';
+
+use Fpp\FppClient;
+
+
+$client = new FppClient($apiKey, $apiSecret, $host);
+
+$data = array(
+    'image_url' => "https://www.faceplusplus.com.cn/scripts/demoScript/images/demo-pic10.jpg",
+    'return_landmark' => '2',
+    'return_attributes' => 'age,headpose'
+);
+
+$resp = $client->detectFace($data);
+var_dump($resp);
 ?>
