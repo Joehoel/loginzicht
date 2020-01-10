108491d82d79a779dad30c0393aaca99b42caf46
Jampottie <j.mooibroek2002@gmail.com>
2020-01-10 12:00:23 +0100

    changed index.php

--- server/index.php
+++ server/index.php
@@ -1,4 +1,20 @@
 <?php 
-$API_KEY = "0zS0ftbUTfv7o1KOlBSoTgKbkYg1ft5u";
-$API_SECRET =  "DVCymhTIoHwLSxqBM9E8riDIxifCQCOS";
+include_once 'FppClient.php';
+
+use Fpp\FppClient;
+
+$host = 'https://api-cn.faceplusplus.com';
+$apiKey = '0zS0ftbUTfv7o1KOlBSoTgKbkYg1ft5u';
+$apiSecret = 'DVCymhTIoHwLSxqBM9E8riDIxifCQCOS';
+
+$client = new FppClient($apiKey, $apiSecret, $host);
+
+$data = array(
+    'image_url' => "https://www.faceplusplus.com.cn/scripts/demoScript/images/demo-pic10.jpg",
+    'return_landmark' => '2',
+    'return_attributes' => 'age,headpose'
+);
+
+$resp = $client->detectFace($data);
+var_dump($resp);
 ?>
