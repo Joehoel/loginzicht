194c9174f5d3b736565f36bc484599c00acac7e5
Jampottie <j.mooibroek2002@gmail.com>
2020-01-21 11:25:19 +0100

    did a lot of shit

--- client/js/main.js
+++ client/js/main.js
@@ -1,10 +1,46 @@
+// Set constraints for the video stream
+var constraints = { video: { facingMode: 'user' }, audio: false };
+var track = null;
+
+// Define constants
+const cameraView = document.querySelector('#camera--view'),
+	cameraOutput = document.querySelector('#camera--output'),
+	cameraSensor = document.querySelector('#camera--sensor'),
+	cameraTrigger = document.querySelector('#camera--trigger');
+
+// Access the device camera and stream to cameraView
+function cameraStart() {
+	navigator.mediaDevices
+		.getUserMedia(constraints)
+		.then(function(stream) {
+			track = stream.getTracks()[0];
+			cameraView.srcObject = stream;
+		})
+		.catch(function(error) {
+			console.error('Oops. Something is broken.', error);
+		});
+}
+
+// Take a picture when cameraTrigger is tapped
+cameraTrigger.onclick = function() {
+	cameraSensor.width = cameraView.videoWidth;
+	cameraSensor.height = cameraView.videoHeight;
+	cameraSensor.getContext('2d').drawImage(cameraView, 0, 0);
+	cameraOutput.src = cameraSensor.toDataURL('image/webp');
+	cameraOutput.classList.add('taken');
+	// track.stop();
+};
+
+// Start the video stream when the window loads
+window.addEventListener('load', cameraStart, false);
+
 const imageUpload = document.getElementById('imageUpload');
 
-Promise.all([
-	faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
-	faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
-	faceapi.nets.ssdMobilenetv1.loadFromUri('/models')
-]).then(start);
+// Promise.all([
+// 	faceapi.nets.faceRecognitionNet.loadFromUri('./models'),
+// 	faceapi.nets.faceLandmark68Net.loadFromUri('./models'),
+// 	faceapi.nets.ssdMobilenetv1.loadFromUri('./models')
+// ]).then(start);
 
 async function start() {
 	console.log('loading...');
